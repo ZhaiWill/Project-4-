f4a7fa604056543aef3b06d3b1eeab38aec5c53b
FARHAN_INSP16\fhabi <85850125+farhan-habib@users.noreply.github.com>
2023-11-09 15:38:13 -0500

    Some more reformatting

--- cs180_project4/src/output.java
+++ cs180_project4/src/output.java
@@ -1,9 +1,11 @@
 public class output {
     private static boolean debugMode = true;
-    public static void print(String message){
-            System.out.println(message);
+
+    public static void print(String message) {
+        System.out.println(message);
     }
-    public static void debugPrint(String message){
-        if(debugMode) System.out.println("\u001B[31mDEBUG: \u001B[0m" + message);
+
+    public static void debugPrint(String message) {
+        if (debugMode) System.out.println("\u001B[31mDEBUG: \u001B[0m" + message);
     }
 }
