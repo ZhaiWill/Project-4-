af605d8ea8163e1129a7cb1ddbe019850e68f32b
ZhaiWill <143210086+ZhaiWill@users.noreply.github.com>
2023-11-11 00:01:56 -0500

    Update Store.java
    added an arraylist of customers, as well as a way to access them and add more customers.

--- cs180_project4/src/Store.java
+++ cs180_project4/src/Store.java
@@ -2,12 +2,14 @@ import java.util.ArrayList;
 public class Store {
     String name;
     User owner;
+    ArrayList<User> customers;
     ArrayList<Item> items;
     
-    public Store(String name, User owner, ArrayList<Item> items) {
+    public Store(String name, User owner, ArrayList<Item> items, ArrayList<User> customers) {
         this.name = name;
         this.owner = owner;
         this.items = items;
+        this.customers = customers;
     }
     
     public String getName() {
@@ -19,6 +21,7 @@ public class Store {
     public User getOwner() {
         return owner;
     }
+    
     public void setOwner(User owner) {
         this.owner = owner;
     }
@@ -32,5 +35,19 @@ public class Store {
     public void addItem(Item item) {
         this.items.add(item);
     }
+
+    public ArrayList<User> getCustomers() {
+        return customers;
+    }
+    
+    public void addCustomer(User u) {
+        if (u.isType() == userType.CUSTOMER) {
+            this.customers.add(u);
+        }
+        else {
+            System.out.println("Sellers can't be customers!");
+        }
+    }
+
 
 }
