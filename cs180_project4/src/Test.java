f58cdeafdbe0d7487af17c1a19885652780abe43
elijahdouglass <elijahdouglass00@gmail.com>
2023-11-13 17:41:51 -0500

    Made test cases

--- cs180_project4/src/Test.java
+++ cs180_project4/src/Test.java
@@ -1,43 +1,41 @@
-//import java.io.FileNotFoundException;
-//import java.io.IOException;
-//import java.util.ArrayList;
-//
-//public class Test {
-//    public static void main(String[] args) {
-//        db.initializeDatabase();
-//
-//        User user1 = User.createUser(userType.CUSTOMER, "john123", "abc123!!", "random@gmail.com");
-//        User user2 = User.createUser(userType.SELLER, "ANDY", "51242", "random2@gmail.com");
-//        System.out.println(user1);
-//        System.out.println(db.getUser("john123"));
-//
-//        System.out.println(user2);
-//        System.out.println(db.getUser("ANDY"));
-//
-//        assert user1 != null;
-//        assert user2 != null;
-//
-//        Message preSaveMessage = user1.sendmessage(user2, "Hello, how are you?");
-//        System.out.println(preSaveMessage);
-//        System.out.println(db.getMessage(String.valueOf(preSaveMessage.getUuid())));
-//        db.editMessage(preSaveMessage, "new message");
-//        System.out.println(db.getMessage(String.valueOf(preSaveMessage.getUuid())));
-//
-//        //db.editUsername(user2, "john123");
-//        System.out.println("MAINTEST:" + user2);
-//
-//        ArrayList<Item> items = new ArrayList();
-//        Item item = new Item(0, 5, "itemname");
-//        items.add(item);
-//
-//        Store store = new Store("storename", user2, items);
-//        Store store2 = new Store("storename", user2, items);
-//        db.saveStore(store);
-//        db.saveStore(store2);
-//        db.saveItem(store2, item);
-//        db.restockItem(store, "itemname", 2);
-//        db.buyItem(store, "itemname", 56);
-//        System.out.println("TEST: " + db.readItemFromFile(store,"itemname"));
-//        System.out.println(db.readStoreFromFile("storename"));
-//    }
-//}
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        //TEST CASES FOR METHODS ARE HERE
+        db.initializeDatabase();
+        User bob = User.createUser(userType.CUSTOMER, "bob", "123", "1@gmai.com");
+        User joe = User.createUser(userType.SELLER, "joe", "123", "2@gmai.com");
+        User andy = User.createUser(userType.SELLER, "andy", "123", "2@gmai.com");
+        User user1 = User.createUser(userType.CUSTOMER, "john123", "abc123!!", "random@gmail.com");
+        User user2 = User.createUser(userType.SELLER, "MANDY", "51242", "random2@gmail.com");
+        System.out.println(user1);
+        System.out.println(db.getUser("john123"));
+
+        System.out.println(user2);
+        System.out.println(db.getUser("MANDY"));
+
+        assert user1 != null;
+        assert user2 != null;
+
+        bob.setUserBlockStatus("andy", userBlockStatus.BLOCKED);
+        bob.setUserBlockStatus("joe", userBlockStatus.INVISIBLE);
+        
+        db.saveUser(bob);
+        System.out.println(bob.userBlockStatusMap);
+        System.out.println(db.getUser("bob").userBlockStatusMap);
+        db.getUser("andy");
+                 Store s = new Store("store1", "joe");
+                 db.saveStore(s);
+        new UI().run();
+        /*EXCPECTED: MANDY and john123 should be printed twice with the exact same info. Bob should not be able to send messages to andy and vice versa. 
+         * Joe should be completely invisible to Bob (i.e. messages cannot be sent to Joe from bob and vise versa, bob should not be able to search for joe)
+         * All messages should persist and only be accessable by the sender and recipient.  
+         * A user should be able to sign into any existing account or create a new account. Any changes made to this account should be saved permanently. 
+         * Any customers (bob, john123) should not be able to create stores and should be able to view them while all sellers (Joe, MANDY, andy) should be able to create new stores
+         * store1 should be visible
+         */
+    }
+}
